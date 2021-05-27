5535654d7a4d689198089fa605777f82b7615f68
leo espin <35843183+leoEspin@users.noreply.github.com>
2021-05-27 13:28:51 -0400

    will erase all images now, unless those asked to be kept

--- clean_registry.py
+++ clean_registry.py
@@ -1,7 +1,5 @@
 '''
-Removes all images in the registry minus those explicitly asked to be kept. The script will run two passes,
-in the first one only images with the default tag will be deleted and a list of dangling images will be created
-(you can ignore the error messages). In the second pass all remaining images will be erased.
+Removes all images in the registry minus those explicitly asked to be kept. 
 TODO: replace gcloud command calls with docker registry API calls:
 https://cloud.google.com/container-registry/docs/reference/docker-api
 '''
@@ -15,10 +13,8 @@ project = requests.get(
             headers={"Metadata-Flavor": "Google"}).text
 
 def parcero():
-    line0 = 'Removes all images in the registry minus those explicitly asked to be kept. The script will run two passes,\n'
-    line1 = 'in the first one only images with the default tag will be deleted and a list of dangling images will be created\n'
-    line2 = '(you can ignore the error messages). In the second pass all remaining images will be erased.'
-    huyparce = argparse.ArgumentParser(description=line0 + line1 + line2, formatter_class=argparse.RawTextHelpFormatter)
+    line0 = 'Removes all images in the registry minus those explicitly asked to be kept.'
+    huyparce = argparse.ArgumentParser(description=line0, formatter_class=argparse.RawTextHelpFormatter)
     huyparce.add_argument('--names-list', nargs='+', type=str,
                           help='Names of images to be kept in the registry', metavar='image_name')
     huyparce.add_argument('--hostname', type=str,
@@ -38,24 +34,28 @@ def get_images(project: str, names: None, host: str = 'us.gcr.io')-> list:
 
 def try_remove(images: list)-> list:
     individual = []
+    remaining = []
     for image in images:
-        out = subprocess.run(f'gcloud container images delete {image} --force-delete-tags  --quiet',
-                             shell=True)
-        if out.returncode > 0:
-            tmp = subprocess.run(f'gcloud container images list-tags {image}',
-                                 shell=True, capture_output=True).stdout.decode('utf-8').split('\n')
-            digests = [x.split()[0] for x in tmp if re.match('[a-z0-9]+', x[0:12])]
-            if len(digests) > 0:
-                individual.extend([image + '@sha256:' + d for d in digests])
-    return individual
+        tmp = subprocess.run(f'gcloud container images list-tags {image}',
+                             shell=True, capture_output=True).stdout.decode('utf-8').split('\n')
+        digests = [x.split()[0] for x in tmp if re.match('[a-z0-9]+', x[0:12])]
+        if len(digests) > 0:
+            individual.extend([image + '@sha256:' + d for d in digests])
+        
+        while len(individual) > 0:
+            single = individual.pop()
+            out = subprocess.run(f'gcloud container images delete {single} --force-delete-tags  --quiet',
+                                 shell=True)
+            if out.returncode > 0:
+                remaining.append(single)
+            
+    return remaining
     
 
 if __name__ == '__main__':
     arguments = parcero()
     images = get_images(project, arguments.names_list, arguments.hostname)
     remaining_img = try_remove(images)
-    if len(remaining_img) > 0:
-        remaining_img = try_remove(remaining_img)
 
     if len(remaining_img) > 0:
         print('Inspect these remaining images individually:')
